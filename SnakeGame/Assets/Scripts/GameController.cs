6717d21ecee0f72559a851a7b942254de5fb73b2
Skopay <Skopay@users.noreply.github.com>
2018-05-05 06:18:14 +1200

    Revert "Revert "Code commenting, improvements.""
    This reverts commit 0765793f6198d969603b105b2819ac492d1f4ace.

--- SnakeGame/Assets/Scripts/GameController.cs
+++ SnakeGame/Assets/Scripts/GameController.cs
@@ -1,15 +1,11 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
-    public int xBound;
-    public int yBound;
-    public Snake head;
-    public Snake tail;
+    //All the game objects used within the game
     public GameObject foodPrefab;
     public GameObject currentFood;
     public GameObject snakePrefab;
@@ -17,50 +13,66 @@ public class GameController : MonoBehaviour {
     public GameObject wallSouth;
     public GameObject wallEast;
     public GameObject wallWest;
+    public Snake head;
+    public Snake tail;
+
+    //Public variables that'll help with the algorthm of the game
+    //Bounds of which the food will spawn
+    public int xBound;
+    public int yBound;
+    //Increases the size of the Snake
     public int maxSize;
     public int currentSize;
+    //Holds the value of the Snake's movement
     public int NESW;
+    //Used to display the current score on the UI and store the value
     public int score;
-    public float deltaTimer;
-    public Vector2 newPos;
     public Text scoreText;
+    //Custom value for calculating the Snake's speed
+    public float deltaTimer;
+    //Used for placing the Food and Snake object in the correct X and Y coordinate
+    public Vector2 newPos;
 
+
+    //Runs the Hit() script when hit is activated
     private void OnEnable()
     {
-        Snake.hit += hit;
+        Snake.hit += Hit;
     }
-    // Use this for initialization
+    
+    // Use this for initialization. Constantly repeats the TimerInvoke() method
     void Start () {
         InvokeRepeating("TimerInvoke", 0, deltaTimer);
         FoodFunction();
 	}
 
+    //Disables the Hit() script when it has finished running
     private void OnDisable()
     {
-        Snake.hit -= hit;
+        Snake.hit -= Hit;
     }
 
-    // Update is called once per frame
+    // Update is called once per frame. Checks for update on Snake's movement
     void Update () {
-        ComChangeD();
+        KeyboardChangeDir();
 	}
 
+    //Moves the Snake each frame by adding a 'head' in the chosen direction and removing the 'tail' as it moves.
     void TimerInvoke()
     {
         Movement();
-        StartCoroutine(CheckVisable());
+        //StartCoroutine(CheckVisable());
         if(currentSize >= maxSize)
         {
             TailFunction();
         }else
         {
             currentSize++;
-            
         }
     }
 
-    void Movement()
-    {
+    //Calculates the button pressed and moves the Snake accordingly
+    void Movement() {
         GameObject temp;
         newPos = head.transform.position;
 
@@ -86,8 +98,8 @@ public class GameController : MonoBehaviour {
         return;
     }
 
-
-    void ComChangeD()
+    //Movement method for keyboard based devices
+    void KeyboardChangeDir()
     {
         if(NESW != 2 && Input.GetKeyDown(KeyCode.W))
         {
@@ -107,7 +119,8 @@ public class GameController : MonoBehaviour {
         }
     }
 
-    public void MobChangeD(int direction)
+    //Movement method for mobile devices
+    public void MobileChangeDir(int direction)
     {
         if (NESW != 2 && direction == 0)
         {
@@ -127,6 +140,7 @@ public class GameController : MonoBehaviour {
         }
     }
 
+    //Moves the tail of the Snake.
     void TailFunction()
     {
         Snake tempSnake = tail;
@@ -134,53 +148,44 @@ public class GameController : MonoBehaviour {
         tempSnake.RemoveTail();
     }
 
+    //Creates a new food object in the bounds of the camera
     void FoodFunction()
     {
         int xPos = Random.Range(-xBound, +xBound);
         int yPos = Random.Range(-yBound, +yBound);
 
         currentFood = (GameObject)Instantiate(foodPrefab, new Vector2(xPos, yPos), transform.rotation);
-
-        //StartCoroutine(CheckRender(currentFood));
     }
 
-    //not needed, i think
-    /*IEnumerator CheckRender(GameObject IN)
-    {
-        yield return new WaitForEndOfFrame();
-        if(IN.GetComponent<Renderer>().isVisible == false)
-        {
-            if(IN.tag == "Food")
-            {
-                Destroy(IN);
-                FoodFunction();
-            }
-        }
-    }*/
-
-    void hit(string WhatWasSent)
+    //Executes code depending on what object the Snake hit
+    void Hit(string WhatWasSent)
     {
         if(WhatWasSent == "Food")
         {
+            //Increases Snake speed to a limit
             if (deltaTimer > .10000f)
             {
                 deltaTimer -= .0125000f;
+                float haultMovement = deltaTimer;
                 CancelInvoke("TimerInvoke");
-                InvokeRepeating("TimerInvoke", 0, deltaTimer);
+                InvokeRepeating("TimerInvoke", haultMovement, deltaTimer);
             }
             FoodFunction();
             maxSize++;
             score++;
             scoreText.text = score.ToString();
+            //Compares the current highscore to the current score and updates it if there is a change
             int temp = PlayerPrefs.GetInt("HighScore");
-            if(score > temp)
+            if (score > temp)
             {
                 PlayerPrefs.SetInt("HighScore", score);
             }
         }
+        //Ends game if obstacle is hit
         if(WhatWasSent == "Snake" || WhatWasSent == "Wall")
         {
-            /*if (powerUpInvis == true)
+            //Idea for 'power up' code
+            /*if (powerUpInvis == true) 
             {
                 return;
                 
@@ -190,12 +195,18 @@ public class GameController : MonoBehaviour {
         }
     }
 
-    //makes it boundless, other map idea
+    //Upon clicking the exit button you will be returned to the Main Menu
+    public void Exit()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    //Two methods that manipulate the camera to mimick the Snake going through one side of the screen and coming out opposite side.
     /*void Wrap()
     {
         if(NESW == 0)
         {
-            head.transform.position = new Vector2(head.transform.position.x, -(head.transform.position.y - 1));
+            head.transform.position = new Vector2(tail.transform.position.x, -(head.transform.position.y - 1));
         }
         if (NESW == 1)
         {
@@ -209,20 +220,14 @@ public class GameController : MonoBehaviour {
         {
             head.transform.position = new Vector2(-(head.transform.position.x + 1), head.transform.position.y);
         }
-    }*/
-
+    }
+    
     IEnumerator CheckVisable()
     {
         yield return new WaitForEndOfFrame();
         if (!head.GetComponent<Renderer>().isVisible)
         {
-            CancelInvoke("TimerInvoke");
-            Exit();
+            Wrap();
         }
-    }
-
-    public void Exit()
-    {
-        SceneManager.LoadScene(0);
-    }
+    }*/
 }
